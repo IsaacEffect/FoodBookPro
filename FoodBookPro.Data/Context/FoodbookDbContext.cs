85bc5931788cae9ce9e070b9c57382a420e6aff6
IsaacEffect <isaacmartinez9875@gmail.com>
2026-02-09 19:51:07 -0400

    context

---
+++ FoodBookPro.Data/Context/FoodbookDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FoodBookPro.Data.Context
+{
+    public class FoodbookDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("FoodbookDb");
+        }
+    }
+}
