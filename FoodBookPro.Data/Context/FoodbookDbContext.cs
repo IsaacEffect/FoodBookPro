c6c5e65faae23c89a5e77122a964624e2e9303f7
Jose Arturo <joseearturo04@gmail.com>
2026-02-21 10:51:08 -0400

    Agregando modelos de datos y controlador para el ticket XAV-53 (Procesar Pago

--- FoodBookPro.Data/Context/FoodbookDbContext.cs
+++ FoodBookPro.Data/Context/FoodbookDbContext.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using FoodBookPro.Data.Entities; // Para que reconozca tus clases
 
 namespace FoodBookPro.Data.Context
 {
     public class FoodbookDbContext : DbContext
     {
+        // Aquí registramos nuestras "tablas"
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // Usamos una base de datos en memoria aun no he configurado el sql
             optionsBuilder.UseInMemoryDatabase("FoodbookDb");
         }
     }
---
+++ FoodBookPro.Data/Entities/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FoodBookPro.Data.Entities
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; } // Para el resumen de orden
+
+        public decimal Subtotal { get; set; }
+        public decimal TipAmount { get; set; } // Criterio: Opción de propina
+        public decimal TotalAmount => Subtotal + TipAmount; // Calculado
+
+        public string PaymentMethod { get; set; } // Criterio: Selección de método
+        public DateTime TransactionDate { get; set; }
+
+        public bool IsSuccess { get; set; } // Criterio: Confirmación exitosa/fallida
+        public string ErrorMessage { get; set; } // Criterio: Manejo de errores
+    }
+}
