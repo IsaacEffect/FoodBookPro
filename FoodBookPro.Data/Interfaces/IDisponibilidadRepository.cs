43f6d55fa367ed6458b296b518fc88eec1bf4e28
Joslam Jerez Perez <20240266@itla.edu.do>
2026-02-24 17:11:13 -0400

    Inteface IDisponibilidadRepository, creada

---
+++ FoodBookPro.Data/Interfaces/IDisponibilidadRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IDisponibilidadRepository
+{
+    Task<DisponibilidadRestaurante?> ObtenerPorDiaAsync(DayOfWeek diaSemana);
+    Task<IEnumerable<DisponibilidadRestaurante>> ObtenerTodasAsync();
+    Task<DisponibilidadRestaurante> CrearAsync(DisponibilidadRestaurante disponibilidad);
+    Task<DisponibilidadRestaurante> ActualizarAsync(DisponibilidadRestaurante disponibilidad);
+}
