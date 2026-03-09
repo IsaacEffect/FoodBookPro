8f334a49f06f24a05095052d26e58d2b7de1e3e7
IsaacEffect <isaacmartinez9875@gmail.com>
2026-03-08 23:39:47 -0400

    XAV-45 Add DbSets for Restaurant and Payment entities
    Added Restaurants and Payments DbSet properties to FoodbookDbContext, enabling management and querying of these entities within the EF context.

--- FoodBookPro.Data/Context/FoodbookDbContext.cs
+++ FoodBookPro.Data/Context/FoodbookDbContext.cs
@@ -17,6 +17,8 @@ namespace FoodBookPro.Data.Context
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<OrderStatusHistory> OrderStatusHistories { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Payment> Payments { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
