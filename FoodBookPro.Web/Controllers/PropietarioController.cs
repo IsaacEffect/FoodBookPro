3a63aceba01651c0c922240b58843867d2c63a57
IsaacEffect <isaacmartinez9875@gmail.com>
2026-03-09 02:01:35 -0400

    XAV-110 Crear modelo Propietario y endpoint de registro
    Add Propietario entity, enum, and controller
    
    Introduced the Propietario entity with restaurant name and status fields, using the new EstadoPropietario enum. Registered Propietarios DbSet in FoodbookDbContext. Added PropietarioController with a basic Index action. Set default status to "Pendiente" for new owners.

---
+++ FoodBookPro.Web/Controllers/PropietarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodBookPro.Web.Controllers
+{
+    public class PropietarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
